608cad741df0b665a00ba68f6f7276875e2a971f
Dalton Lang <35282898+GrammAcc@users.noreply.github.com>
2024-10-25 14:03:01 -0500

    Fix Filename Errors in Env Override Test Case
    The test case for checking the env override behavior between the
    `DEFAULT_GRAMMLOG_LEVEL` and the `GRAMMLOG_LEVEL` creates a random
    filename to ensure it a log file created from another test case doesn't
    interfere with the results of the test. But I was using a stupid method
    of generating the name. For some reason, I stringified random bytes and
    used that result directly as the filename. I don't know why I thought
    that would work as a filename in all cases, but regardless, the Windows
    CI runner failed last night when I pushed the initial upload, and I
    assumed that was because windows makes everything difficult (which it
    normally does), but this one was all on me.
    
    I have updated the test case to use 8 random alpha characters instead of
    random bytes, and I also reenabled the Windows runner, so hopefully
    everything will pass now. I don't have a local windows machine to test
    this on though.
    
    Note:
        I may eventually start using a temporary directory for the log files
        in the test suite, but I prefer keeping the test environment as
        close to production as possible, so for the time being, I will
        continue to create actual log files in the test suite and simply
        gitignore them. Once I have a complete regression suite with test
        cases for all of the invariants of the library, I will probably look
        into making things more efficient.

--- test/env_test.py
+++ test/env_test.py
@@ -1,6 +1,7 @@
 import os
 import os.path
 import random
+import string
 from pathlib import Path
 
 import grammlog
@@ -9,10 +10,11 @@ import grammlog
 def test_log_level_override(fixt_default_logger):
     """Ensure that the DEFAULT_GRAMMLOG_LEVEL can be overriden with the GRAMMLOG_LEVEL env var."""
 
+    print("from the test suite")
     grammlog.debug(fixt_default_logger, "test message")
     assert os.path.getsize("logs/default.log") == 0
     os.environ["GRAMMLOG_LEVEL"] = "DEBUG"
-    rand_name = str(random.randbytes(10))
+    rand_name = "".join([random.choice(string.ascii_letters) for _ in range(8)])
     new_logger = grammlog.make_logger(rand_name)
     grammlog.debug(new_logger, "test message")
     data = Path(f"logs/{rand_name}.log").read_text()
